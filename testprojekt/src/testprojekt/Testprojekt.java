21464e727eb334409d11a66cf935db61b512ccc0
Jörg <Jrg@Joerg-PC>
2012-10-29 18:35:14 +0100

    testcoomit

---
+++ testprojekt/src/testprojekt/Testprojekt.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojekt;
+
+/**
+ *
+ * @author Jörg
+ */
+public class Testprojekt {
+
+    /**
+     * @param args the command line arguments
+     */
+    //bla
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
